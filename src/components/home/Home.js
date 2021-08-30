13cdd2d5fa446e8fc12e06b169f12a3c81ddfa44
Elir25 <elirendiles@gmail.com>
2021-08-30 22:27:00 +0200

    home page basic HTML rendered

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './home.css'
 const Home = () => {
     return (
         <>
-        <h1>HOME</h1>
+        <h1 className="home-tittle">ELIALBA RENDILES</h1>
+        <h3 className="home-sub-t">Web developer / Graphic designer</h3>
+        <div className="social-list-container">
+        <ul className="social-list">
+            <li><i className="fab fa-github"></i></li>
+            <li><i className="fab fa-linkedin"></i></li>
+            <li><i className="fas fa-envelope"></i></li>
+            {/* <li><i className="fab fa-behance"></i></li>
+            <li><i className="fab fa-dribbble"></i></li> */}
+        </ul>
+        </div>
         </>
     )
 }
